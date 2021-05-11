5935e2ad06c64ea0119123a1b8fc5813c3264847
NataliaSacias <nsacias@gmail.com>
2021-05-11 17:57:05 +0000

    fetch funcionando

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,12 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img
+					src="https://i.pinimg.com/originals/30/6f/f9/306ff98466757b3a21f34283c4217eeb.jpg"
+					width="80"
+					height="60"
+				/>
+				{/* <span className="navbar-brand mb-0 h1">React Boilerplate</span> */}
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
