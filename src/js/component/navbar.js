89467c350e9e13ed23b09f14a7a377fddf22fb9d
NataliaSacias <nsacias@gmail.com>
2021-05-14 00:25:28 +0000

    navbar fixed

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,7 +6,8 @@ export const Navbar = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-light mb-3 navbar-default fixed-top">
+			{/* <div className="container"> */}
 			<Link to="/">
 				<img
 					src="https://i.pinimg.com/originals/30/6f/f9/306ff98466757b3a21f34283c4217eeb.jpg"
@@ -20,8 +21,8 @@ export const Navbar = () => {
 					<button className="btn btn-primary">Check the Context in action</button>
 				</Link>
 			</div> */}
-			<span>Que la Fuerza te acompañe</span>
-			<div className="dropdown pull-left">
+			<span className="titulo">Que la Fuerza te acompañe</span>
+			<div className="dropdown">
 				<button
 					className="btn btn-secondary dropdown-toggle"
 					type="button"
@@ -29,26 +30,25 @@ export const Navbar = () => {
 					data-toggle="dropdown"
 					aria-haspopup="true"
 					aria-expanded="false">
-					Favoritos
+					{store.favoritos == 0 ? `No hay favoritos` : `Favoritos ${store.favoritos.length}`}
 				</button>
-				<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<div className="dropdown-menu dropdown-menu-lg-right" aria-labelledby="dropdownMenuButton">
 					{store.favoritos.map((favorito, i) => {
 						return (
-							<li key={i}>
+							<li key={i} className="d-flex justify-content-between">
 								{favorito}
-								<button
+								<i
 									onClick={() => {
 										actions.eliminarFavorito(i);
 									}}
-									type="button"
-									className="btn btn-outline-danger">
-									<i className="fas fa-trash-alt" />
-								</button>
+									className="fas fa-trash-alt"
+								/>
 							</li>
 						);
 					})}
 				</div>
 			</div>
+			{/* </div> */}
 		</nav>
 	);
 };
