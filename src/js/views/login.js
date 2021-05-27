17411acf77b6d8bcdc126f5fa9553f3d7b4c469f
NataliaSacias <nsacias@gmail.com>
2021-05-27 15:47:45 +0000

    CREATE/view login

---
+++ src/js/views/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.scss";
+
+
+export const Login = () => (
+
+    return(
+	<div>
+		
+	</div>
+));
